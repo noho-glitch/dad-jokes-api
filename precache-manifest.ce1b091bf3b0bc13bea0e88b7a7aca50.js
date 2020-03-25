c2927ae0a259b120f2358b22db979353fe328a55
Dave Nolan <dpn.nolan@gmail.com>
2020-03-24 23:26:04 -0400

    Updates

---
+++ precache-manifest.ce1b091bf3b0bc13bea0e88b7a7aca50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fe9c17876799b5dfeb9af49c99a1a9",
+    "url": "/dad-jokes-api/index.html"
+  },
+  {
+    "revision": "209418206828368c029d",
+    "url": "/dad-jokes-api/static/css/main.239f6c7d.chunk.css"
+  },
+  {
+    "revision": "f00cbf571e249ec313eb",
+    "url": "/dad-jokes-api/static/js/2.0c54f90c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes-api/static/js/2.0c54f90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209418206828368c029d",
+    "url": "/dad-jokes-api/static/js/main.8ecf3f5d.chunk.js"
+  },
+  {
+    "revision": "5e5f5c052f2bf82d6865",
+    "url": "/dad-jokes-api/static/js/runtime-main.3efae4f8.js"
+  }
+]);
